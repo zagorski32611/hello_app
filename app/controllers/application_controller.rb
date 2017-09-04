9734198ccb0ae1d9ea39d00f91f42334404bf7cb
zagorski32611 <j.j.zagorski@gmail.com>
2017-09-04 12:52:56 -0400

    added pg gem to gemfile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "Hola Mundo!"
+    render html: "Hello world!!"
   end
 
   def bye
